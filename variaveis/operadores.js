0a8e5b8f5750cb9b9bfcb9ce709191edc2532d19
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-01-25 11:11:54 -0300

    realizando operações matematicas com operadores aritmeticas

---
+++ variaveis/operadores.js
@@ -0,0 +1,15 @@
+// Realizando contas com operadores aritméticos
+
+let x = 10, y = 2;
+let resultado = x + y; // Adição
+
+console.log(resultado); // 12
+
+resultado = x - y; // Subtração
+console.log(resultado); // 8
+
+resultado = x * y; // Multiplicação
+console.log(resultado); // 20
+
+resultado = x / y; // Divisão
+console.log(resultado); // 5
