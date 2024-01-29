30081a841fba985a5621310a3947752eedb27a31
xArthur29 <arthurgruimaraes@gmail.com>
2024-01-29 20:22:16 -0300

    praticando

--- condicionais/condicionais.js
+++ condicionais/condicionais.js
@@ -5,16 +5,22 @@
     Tem ingresso? sim.
     É maior de 16 anos? sim.
     Então: Pode ver o filme.
+    É menor de idade e está com os pais?
+    Então: Pode ver o filme
     Se não: Não pode ver o filme.
 */
 
 const temIngresso = true;
 const censura = 16;
 const idade = 18;
+const estaComOsPais = false;
 
-if (temIngresso === true && idade >= censura) {
-    console.log("Tudo ok para ver o filme.");
+if (temIngresso) {
+    if (idade >= censura || estaComOsPais) {
+        console.log("Pode ver o filme.")
+    } else {
+        console.log("Não pode ver o filme.");
+    }
 } else {
     console.log("Não pode ver o filme.");
 }
-
