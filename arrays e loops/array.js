39502b3b1f87cbe92111eb84d8797dc28dea9145
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-01-31 19:16:16 -0300

    praticando com arrays

---
+++ arrays e loops/array.js
@@ -0,0 +1,11 @@
+// Arrays: uma lista que guarda um ou mais valores dentro dele
+// ex:
+//    const array = [1, 2, 3];
+// OBS: O tamanho do array é sempre maior do que o número de indices que o mesmo contém.
+// Tamanho do array: 3
+// Indices: 2
+// por que? o primeiro índice de um array é sempre zero.
+
+const nomesEstudantes = ["Julio", "Arthur", "Guto", "Eduardo", "Luis"];
+
+console.log(nomesEstudantes);
