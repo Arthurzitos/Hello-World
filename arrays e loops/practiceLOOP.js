c0bec011fe972c9216fa4b8fa3a7751bc7e8b52c
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-05 17:24:02 -0300

    exerćicio resolvido utilizando for

---
+++ arrays e loops/practiceLOOP.js
@@ -0,0 +1,12 @@
+// Praticando com loops em js
+
+/* 
+    Faça um programa que imprima todos os elementos
+    de um ARRAY que contém alguns números quaisquer.
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
