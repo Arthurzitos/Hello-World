2dcd4d7036292081d9be28cb97210c652e54373f
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-05 17:28:00 -0300

    boas praticas com nome da variavel

--- arrays e loops/practiceLOOP.js
+++ arrays e loops/practiceLOOP.js
@@ -7,6 +7,6 @@
 
 const numbers = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+for (let indice = 0; indice < numbers.length; indice++) {
+    console.log(numbers[indice]);
 }
