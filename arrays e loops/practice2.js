15e232870156c1293f4e516432a3f59ad9ae1960
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-06 19:16:24 -0300

    praticando com for

---
+++ arrays e loops/practice2.js
@@ -0,0 +1,17 @@
+// Faça um programa que conte de 10 até 0
+// e imprima na tela a contagem
+// obs: não vale criar um array e imprimi-lo direto na tela
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+/*
+    Primeiro: reservamos o valor 10 em uma variavel
+
+    segundo: enquanto o valor da variavel for maior ou igual a 0, continuamos diminuindo
+
+    terceiro: imprime a contagem de 10 até 0 em tela
+*/
+
+
