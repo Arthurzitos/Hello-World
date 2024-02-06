1dd174996a36416d5002ebf6ce9ff6471af3d143
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-06 19:26:27 -0300

    praticando mais com loops

---
+++ arrays e loops/practice3.js
@@ -0,0 +1,10 @@
+// criar um programa que imprime todos os
+// números pares de 1 a 50;
+
+let numero = 2; 
+console.log(numero);
+
+while (numero <= 50) {
+    console.log(numero);
+    numero += 2;
+}
