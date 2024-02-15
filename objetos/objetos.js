233188ed9abf56b619c5a44c3c8aa1705c09e33f
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-15 15:29:19 -0300

    praticando um pouco com objetos

---
+++ objetos/objetos.js
@@ -0,0 +1,10 @@
+// iniciando com objetos
+
+const pessoa = {
+    nome: 'fulano',
+    altura: 1.80,
+    peso: 70
+}; // criando um objeto com  algumas propriedades dentro
+
+// console.log(pessoa); // imprimindo o objeto inteiro em tela
+//console.log(`Olá, meu nome é ${pessoa.nome}`); // imprimindo somente uma propriedade do objeto em tela
