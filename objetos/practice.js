164cc3420d4ee1696d0ca4488dcad5e3dc5d6d9c
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-19 18:54:20 -0300

    adicionado enunciado do exercicio

---
+++ objetos/practice.js
@@ -0,0 +1,10 @@
+/*
+    Declare uma variável que armazene um OBJETO
+    contendo as seguintes PROPRIEDADES:
+    Nome, idade, altura, se tem CNH e apelidos.
+
+    Depois faça um programa que imprime na tela o belo texto abaixo,
+    substituindo os dados pessoais pelos dados do objeto
+
+    "José tem 30 anos, 1.73m de altura, possui CNH e os seguintes apelidos: Jr, Juninho".
+*/
