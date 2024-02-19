75ad46f6ebb95894f3adece6af90069951f8c167
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-19 20:01:51 -0300

    criando objeto dentro de objeto

---
+++ objetos/shorthand.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Arthur",
+    altura: 1.78,
+    carro: {
+        marca: "Toyota",
+        modelo: "Etios",
+        ano: 2020,
+        potencia: 100
+    } // criado objeto dentro do objeto principal
+
+}; // criado objeto principal
+
+console.log(pessoa); // imprimindo em tela
