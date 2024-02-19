c6fb8c85426e188b171cf9d1efa03db3c636087a
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-19 19:29:48 -0300

    praticando com objetos

--- objetos/practice.js
+++ objetos/practice.js
@@ -8,3 +8,26 @@
 
     "José tem 30 anos, 1.73m de altura, possui CNH e os seguintes apelidos: Jr, Juninho".
 */
+
+const pessoa = {
+    nome: "Junior",
+    idade: 30,
+    altura: 1.73,
+    temCNH: true,
+    apelidos: ["Jr", "Juninho"]
+} // criando objeto "pessoa" com as propriedades solicitadas
+
+let textoCNH = "";
+
+if (pessoa.temCNH === true) {
+    textoCNH = "possui CNH";
+} else {
+    textoCNH = "não possui CNH";
+} // verificando se a pessoa possui CNH
+
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos, ${pessoa.altura}m, ${textoCNH} e seus apelidos são: 
+- ${pessoa.apelidos[0]}
+- ${pessoa.apelidos[1]}`)
+// imprimindo resposta em tela
+    
+
