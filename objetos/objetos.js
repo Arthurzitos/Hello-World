8ffa56155af8cbfb09859a2c34ff4fa8353f0060
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-02-19 18:46:26 -0300

    continuando

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -6,9 +6,9 @@ const pessoa = {
     peso: 70
 }; // criando um objeto com  algumas propriedades dentro
 
+console.log(pessoa); // imprimindo o objeto inteiro em tela
+
+console.log(`Olá, meu nome é ${pessoa.nome}`); // imprimindo somente uma propriedade do objeto em tela
+
 pessoa.nome = 'ciclano'; // alterando o valor da propriedade "nome"
 console.log(pessoa.nome); // imprimindo o novo valor da propriedade "nome"
-
-
-// console.log(pessoa); // imprimindo o objeto inteiro em tela
-//console.log(`Olá, meu nome é ${pessoa.nome}`); // imprimindo somente uma propriedade do objeto em tela
