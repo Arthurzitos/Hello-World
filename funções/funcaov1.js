22edc9601981f61f3ca9975ead924403246b2f75
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-03-04 16:09:32 -0300

    começando com funções

---
+++ funções/funcaov1.js
@@ -0,0 +1,15 @@
+// o que é uma função?
+
+/*
+    é um bloco de código que executa uma ou mais instruções
+    quantas vezes forem necessárias
+*/
+
+// criar uma função que imprima "Olá Mundo" na tela.
+
+function olaMundo() {
+    console.log("Olá Mundo!");
+}
+
+olaMundo(); // chamando a função
+
