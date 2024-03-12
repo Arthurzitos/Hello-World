f226634001ec19579037a73e2fdff2887f3a56a7
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-03-12 10:00:11 -0300

    arrow function

---
+++ funções/arrowFunction.js
@@ -0,0 +1,15 @@
+function soma (x, y) {
+    // codigo
+    const resultado = x + y;
+    return resultado;
+}
+
+// utilizando arrow function
+
+const soma = (x, y) => {
+    // codigo
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5, 2)); // retorno da função
