175c644f663ec351c03b3b4075b9c32df7d28cbe
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-03-12 15:44:05 -0300

    iniciando com metodos

---
+++ funções/metodos.js
@@ -0,0 +1,13 @@
+// função anonima
+
+const pessoa {
+    nome: 'nome',
+    idade: 'idade',
+    profissao: 'profissao',
+    
+    apresentar: function () {
+        const faixaEtaria =  determinarFaixaEtaria(pessoa.idade);
+
+        console.log(faixaEtaria
+    }
+}
