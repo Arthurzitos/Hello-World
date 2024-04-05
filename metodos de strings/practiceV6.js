770f586f7fd520ef1b4677de52eb3ca4ae6883bd
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-05 20:53:34 -0300

    praticando com metodo trim

---
+++ metodos de strings/practiceV6.js
@@ -0,0 +1,8 @@
+//  trim()
+// remove os espaços em branco tanto no inicio quanto no fim de uma string
+
+// ex:
+
+const email = "   email@example.com  "; // string com espaços em branco
+const formatado = email.trim(); // removendo os espaços em branco
+console.log(`_${formatado}_`); // _Utilizando para poder visualizar os espaços em branco removidos_
