a0cd7afd1cddaa44a6ce789873aa55222c8c2f63
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-05 20:47:08 -0300

    praticando mais

---
+++ metodos de strings/practiceV5.js
@@ -0,0 +1,17 @@
+// toUpperCase()
+// coloca todas as letras de uma determinada string em MAIUSCULAS
+
+// e...
+
+// toLowerCase()
+// coloca todas as letras de uma determinada string em MINUSCULAS
+
+const textoMaiusculo = "TEXTO EM MINUSCULO";
+const toLowerCase = textoMaiusculo.toLowerCase();
+
+console.log(toLowerCase);
+
+const textoMinusculo = "texto em maiusculo";
+const toUpperCase = textoMinusculo.toUpperCase();
+
+console.log(toUpperCase);
