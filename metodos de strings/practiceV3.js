c42bba5146327312b19eff56ad53ebc5fa3da25c
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-05 20:07:08 -0300

    praticando com replace

---
+++ metodos de strings/practiceV3.js
@@ -0,0 +1,10 @@
+// praticando com REPLACE
+
+// faça um programa que altere uma string que contenha "." para ","
+// ex:
+//  12.90 deve ser 12,90
+
+const numero = "12.90";
+const numeroAlterado = numero.replace(".", ",");
+
+console.log(numeroAlterado);
