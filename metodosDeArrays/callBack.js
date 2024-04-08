4d04a3b252e7aed83f55478d2b525e671e78bff4
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-08 18:37:51 -0300

    praticando callback

---
+++ metodosDeArrays/callBack.js
@@ -0,0 +1,15 @@
+// callback function
+
+const imprimirNome = () => {
+    console.log("Arthur Guimaraes")
+}
+
+// setTimeout(imprimirNome, 1000)
+
+setTimeout (() => {
+    console.log("Cubos Academy");
+}, 1000)
+
+setTimeout ( function() {
+    console.log("teste de função");
+}, 1000)
