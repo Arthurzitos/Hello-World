f2b1e767390e879ec262011a234099bff1d04b85
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-20 02:15:28 -0300

    ordenando objetos com subtração

--- metodosDeArrays/practiceV3.js
+++ metodosDeArrays/practiceV3.js
@@ -1,30 +1,14 @@
 const numeros = [3, 20, 13, 50, 1, 4, 2, 13];
 // ordenação de elementos em ordem CRESCENTE
 numeros.sort((a, b) => {
-    if (a < b) {
-        return -1;
-    }
-
-    if (a > b) {
-        return 1;
-    }
-
-    return 0;
+    return (a - b);
 });
 
 console.log(numeros);
 
 // ordenação de elementos em ordem DECRESCENTE
 numeros.sort((a, b) => {
-    if (a < b) {
-        return 1;
-    }
-
-    if (a > b) {
-        return -1;
-    }
-
-    return 0;
+    return (b - a);
 });
 
 console.log(numeros);
