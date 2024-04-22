ded6e79a5fe77f980693f55e7a5c487bd6b9b85e
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-21 23:31:24 -0300

    praticando com importação e exportação de arquivos

--- servidor/index.js
+++ servidor/index.js
@@ -1,6 +1,7 @@
 const {uniq} = require('lodash');
+const array = require('./array.js');
 
-const array = [1, 2, 4, 4, 6, 7, 1];
+//const array = [1, 2, 4, 4, 6, 7, 1];
 
 const arrayUnico = uniq(array);
 
