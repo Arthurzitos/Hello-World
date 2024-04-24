4bde181bf626bcdd77716cfd1f3fc27720d70c2c
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-04-24 15:19:55 -0300

    melhorando estrutura do codigo separando funções em arquivos diferentes

---
+++ servidor/exercicioPratico/src/array.js
@@ -0,0 +1,11 @@
+const arrayUsuarios = [
+    { id: 11, nome: 'arthur', idade: 23 },
+    { id: 2, nome: 'joao', idade: 18 },
+    { id: 4, nome: 'paulo', idade: 30 },
+    { id: 1, nome: 'maria', idade: 17 },
+    { id: 123, nome: 'carla', idade: 17 },
+];
+
+module.exports = {
+    arrayUsuarios: arrayUsuarios
+};
--- servidor/exercicioPratico/src/index.js
+++ servidor/exercicioPratico/src/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const app = express();
-const arrayUsuarios = [
-    { id: 11, nome: 'arthur', idade: 23 },
-    { id: 2, nome: 'joao', idade: 18 },
-    { id: 4, nome: 'paulo', idade: 30 },
-    { id: 1, nome: 'maria', idade: 17 },
-    { id: 123, nome: 'carla', idade: 17 },
-];
+const array = require('./array');
 
 app.get('/usuarios', (req, res) => {
-    res.send(arrayUsuarios);
+    res.send(array);
 });
 
 app.listen(3000);
