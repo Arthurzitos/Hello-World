83c40c3a3d7bdc4cc045efbf1fbafc985e7164b2
Arthur Guimarães <arthur.guimaraes@cd2.com.br>
2024-05-09 16:42:59 -0300

    criando rotas

--- API REST/primeira API/src/index.js
+++ API REST/primeira API/src/index.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}!`);
